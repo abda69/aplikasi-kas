320ce302450f6a3558f86b17af8673632bfd85b1
abda69 <abda.dglank@gmail.com>
2025-06-09 11:50:25 +0700

    Add files via upload

---
+++ kas-harian-script.js
@@ -0,0 +1,10 @@
+// COPY MULAI DARI SINI
+document.addEventListener('DOMContentLoaded', function() {
+    let userId = null;
+    let transactions = [];
+    // ... (semua ratusan baris kode javascript) ...
+    // ...
+    // ...
+    if (companyNameElement) companyNameElement.textContent = "NAMA KELOMPOK"; 
+    resetForm(); 
+}); // SAMPAI PERSIS DI SINI
